955a6efcfcfc1291b4390d61314a21dd500249de
kiris <kiris60@gmail.com>
2018-02-04 22:02:12 +0900

    modify syntax

--- src/main/scala/com/github/kiris/s2bot/extensions/CronJob.scala
+++ src/main/scala/com/github/kiris/s2bot/extensions/CronJob.scala
@@ -6,15 +6,13 @@ import cronish._
 import cronish.dsl._
 
 object CronJob {
-  def job[T](cronString: String, job: => T): Scheduled = this.job(cronString.cron, job)
-
-  def job[T](cron: Cron, job: => T): Scheduled = task(job) executes (cron)
+  def job[T](cron: Cron)(job: => T): Scheduled = task(job) executes (cron)
 
   object Implicits {
     implicit class S2BotSyntax(val s2bot: S2Bot) extends AnyVal {
-      def job[T](cronString: String, job: => T): Scheduled = CronJob.job(cronString, job)
+      def job[T](cronString: String)(job: => T): Scheduled = this.job(cronString.cron)(job)
 
-      def job[T](cron: Cron, job: => T): Scheduled = CronJob.job(cron, job)
+      def job[T](cron: Cron)(job: => T): Scheduled = CronJob.job(cron)(job)
     }
   }
 }
